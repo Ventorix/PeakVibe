a614a229b599696b320bb456c2dd316318b0c2d3
1Fenix7 <userpk66@gmail.com>
2024-09-14 22:23:40 +0300

    fix config

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,9 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-
 function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <>
-      <h1>count is 123 {count}</h1>
-    </>
-  );
+	return (
+		<>
+			<h1 className='bg-green-600'>Hello</h1>
+		</>
+	);
 }
 
 export default App;
