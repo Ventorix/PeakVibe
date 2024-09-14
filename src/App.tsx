f016914aeb762a57f387def4c78eaceafd35f11b
Magomed-Amin Tochiev <muhammad-amin4@mail.ru>
2024-09-14 21:43:18 +0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1>count is 123 {count}</h1>
+      <h1>count is 123</h1>
     </>
   );
 }
